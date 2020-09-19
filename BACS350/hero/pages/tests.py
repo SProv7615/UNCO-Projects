53b00f89ccb6ec75fba1a92c913d8b88c5d5d860
SProv7615 <prov7615@bears.unco.edu>
2020-09-18 21:16:09 -0600

    Did a test in hero
    Test worked

--- BACS350/hero/pages/tests.py
+++ BACS350/hero/pages/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+
+class ViewTests(SimpleTestCase):
+
+    def test_view_index(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
 
 # Create your tests here.
