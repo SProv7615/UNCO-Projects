dc2829c093c773741d0c83a3725b0ab27bc987c3
SProv7615 <prov7615@bears.unco.edu>
2020-11-19 12:06:14 -0700

    L30

--- BACS350/Lesson27/workshop/urls.py
+++ BACS350/Lesson27/workshop/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 
-from .views import AboutView, HomeView, CardView, CardsView
+from .views import CardView, CardsView, DocumentView, HomeView
 
 
 urlpatterns = [
     
     path('', HomeView.as_view(), name='workshop'),
-    path('about', AboutView.as_view(), name='base'),
     path('card', CardView.as_view(), name='card'),
     path('cards', CardsView.as_view(), name='cards'),
+    path('doc', DocumentView.as_view(), name='doc'),
     
     
 ]
--- BACS350/Lesson27/workshop/views.py
+++ BACS350/Lesson27/workshop/views.py
@@ -1,22 +1,18 @@
 from django.views.generic import TemplateView
+from markdown import markdown
 
-text = '''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae amet consequuntur iure inventore animi. Nihil sunt, aut. Autem obcaecati aliquam nulla rem, quam rerum, ab quidem, doloribus natus dolores voluptatem.'''
+from .workshop import card_data, cards_data, markdown_file_data
+
 
 class HomeView(TemplateView):
     template_name = 'workshop.html'
 
-class BaseView(TemplateView):
-    template_name = 'theme.html'
-
-class AboutView(TemplateView):
-    template_name = 'about.html'
 
 class CardView(TemplateView):
     template_name = 'card.html'
     
     def get_context_data(self, **kwargs):
-        data = dict(title="Cards are Great", body=text)
-        return dict(card=data, title="Card View Layout")
+        return dict(card=card_data())
     
 class CardsView(TemplateView):
     template_name = 'cards.html'
@@ -24,13 +20,17 @@ class CardsView(TemplateView):
     def get_context_data(self, **kwargs):
         return dict(cards=cards_data())
 
-def lorem(num_chars):
-    text = '''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium quibusdam sit hic, ipsum labore commodi eligendi quos culpa maxime voluptate. Ad, voluptatem, esse! Quam accusantium minus sequi cumque minima quod odio accusamus assumenda dolorem consequuntur esse alias nisi, explicabo error! Dolores blanditiis adipisci laboriosam quo sequi nostrum consectetur recusandae illo sed molestias laborum, ullam modi doloremque. Facilis nulla iure odit accusamus. Doloremque voluptatibus repudiandae, beatae temporibus odit suscipit eius facere dolores quibusdam perspiciatis exercitationem velit porro cupiditate repellendus et tempora dolorem, sapiente, debitis cum nihil sunt. Illo perspiciatis cumque qui quia quam quibusdam doloribus fugiat porro, soluta esse nesciunt itaque? Blanditiis, voluptate, voluptates iure iste aut maxime perferendis aliquid, odit doloribus sapiente temporibus ad mollitia consequuntur, alias totam tenetur ut. Accusantium voluptas temporibus, ea a impedit.'''
-    return text[:num_chars]
+class MarkdownView(TemplateView):
+    template_name='markdown.html'
     
-def cards_data():
-    return [
-        dict(title="Card 1", body=lorem(200), color='bg-success'),
-        dict(title="Card 2", body=lorem(150), color='bg-info'),
-        dict(title="Card 3", body=lorem(20), color='bg-primary'),
-    ]
+    def get_context_data(self, **kwargs):
+        md_doc = "README.md"
+        return dict(markdown=markdown_data(md_doc))
+    
+class DocumentView(TemplateView):
+    template_name = 'markdown.html'
+    
+    def get_context_data(self, **kwargs):
+        doc = "README.md"
+        card = dict(title='Doc View', body=markdown(open(doc).read()), color='bg-danger', width='col-lg-12')
+        return dict(card=card)
